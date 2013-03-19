32702c974b459b7f4e4ab23fd847066f8e81ca45
Alex (win) <alex@phpguide.co.il>
2013-03-19 19:07:46 +0200

    basic working demo

---
+++ ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalR
+{
+    public class ChatHub : Hub
+    {
+        public void sendMessage(string msg)
+        {
+            Clients.All.receiveMessage(msg);
+        }
+    }
+}
